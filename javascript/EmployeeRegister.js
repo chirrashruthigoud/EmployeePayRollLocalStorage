651ae3d0094e6ddc49eec31870b08b4373ba5739
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-06 15:08:37 +0530

    Create employee pay roll object

--- javascript/EmployeeRegister.js
+++ javascript/EmployeeRegister.js
@@ -17,7 +17,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
             textError.textContent = e;
         }
     });
-
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent= salary.value;
@@ -25,3 +24,55 @@ window.addEventListener('DOMContentLoaded', (event) => {
     output.textContent = salary.value;
    });
 });
+
+/*AddEmployeePayrol UC-3 => On Save Create Employee Payroll Object */
+ const save=(event)=>
+{
+    try{
+        let employeePayrollData= createEmployeePayroll();
+    }
+    catch(e)
+    {
+        return(e);
+    }
+}
+
+const createEmployeePayroll=()=>{
+    let employeePayrollData=new EmployeePayrollData();
+    try{
+        employeePayrollData.name=getInputValueById('#name');
+    }
+    catch(e)
+    {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+    employeePayrollData.profilePic=getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department=getSelectedValues('[name=department]');
+    employeePayrollData.salary=getInputValueById('#salary');
+    employeePayrollData.note=getInputValueById('#notes');
+    let date=getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollData.date=Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+    }
+
+    const getSelectedValues=(propertyValue)=>{
+    let allItems=document.querySelectorAll(propertyValue);
+    let selItems=[];
+    allItems.forEach(item=>{
+        if(item.checked)selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById=(id) => {
+    let value = document.querySelector(id). value;
+    return value;
+}
+const getInputElementValue =(id) =>
+{
+    let value = document.getElementById(id).value;
+    return value;
+}
